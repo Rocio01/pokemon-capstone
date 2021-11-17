3dec56655a894a2875e4df6cdec90f631fb5f2c3
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-17 16:08:41 -0500

    Add filter for pokemon on the home page and retrieve details functionality

--- src/components/DetailedPokemon.js
+++ src/components/DetailedPokemon.js
@@ -1,10 +1,41 @@
-// import React, { useEffect } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { fetchPokemons } from '../redux/pokemons/pokemons';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
+import { fetchPokemons } from '../redux/pokemons/pokemons';
 
-const DetailedPokemon = () => (
-  <div className="col-sm-6 country-card">
-    <div className="card" />
-  </div>
-);
+const DetailedPokemon = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchPokemons());
+  }, []);
+  const pokemonsState = useSelector((state) => state.pokemonsReducer.pokemons);
+  const { name } = useParams();
+  return (
+
+    <div className="details">
+      <div className="card">
+        {(pokemonsState.filter(((pokemon) => pokemon.name === name)).map((pokemon) => (
+
+          <div className="col-sm-6 country-card" key={pokemon.name}>
+            <div className="card">
+              <img src={pokemon.sprites.front_shiny} className="card-img-top pokemon" alt="pokemon" />
+              <div className="card-body">
+                <h4 className="card-title">
+                  { `Name: ${pokemon.name}`}
+                </h4>
+                <p>
+                  { `Expierence: ${pokemon.base_experience}`}
+                </p>
+
+                <p>
+                  { `Ability: ${pokemon.abilities[0].ability.name}`}
+                </p>
+              </div>
+            </div>
+          </div>
+        )))}
+      </div>
+    </div>
+  );
+};
 export default DetailedPokemon;
