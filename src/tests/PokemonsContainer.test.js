a5f5e18b1aceace0f14462df2dd89eeca0da359a
Zulma Rocio <zrmartinezg@gmail.com>
2021-11-18 19:09:24 -0500

    Add store's tests

---
+++ src/tests/PokemonsContainer.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
+import PokemonsContainer from '../components/PokemonsContainer';
+import store from '../redux/configureStore';
+
+describe('pokemons container component', () => {
+  test('matches MissionsContainer component snapshot', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <PokemonsContainer />
+      </Provider>,
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
---
+++ src/tests/store.test.js
@@ -0,0 +1,37 @@
+import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import pokemonsReducer from '../redux/pokemons/pokemons';
+import '@testing-library/jest-dom/extend-expect';
+import App from '../App';
+import store from '../redux/configureStore';
+
+test('should return the initial state', () => {
+  expect(pokemonsReducer(undefined, {})).toEqual({ pokemons: [] });
+});
+
+test('shows the correct text after the next state from the store that came from PokemonsContainer', async () => {
+  const { findByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  expect(await findByText('DETAILS BY POKEMON')).toBeInTheDocument();
+});
+
+test('shows the correct text after the next state from the store that came form Nav ', async () => {
+  const { findByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  expect(await findByText('catch your pokemon')).toBeInTheDocument();
+});
+
+test('shows the correct text after the next state from the store that came from Pokemon ', async () => {
+  const { findByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
+  expect(await findByText('nidorino')).toBeInTheDocument();
+});
